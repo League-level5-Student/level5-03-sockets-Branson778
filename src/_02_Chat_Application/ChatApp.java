830419f9bfde8f9cadc142235eeb09020a417b95
r_boo <r_boo@192.168.1.102>
2023-04-30 17:53:00 -0700

    Finished Module

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,64 @@
 package _02_Chat_Application;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
-
+public class ChatApp extends JFrame {
+	JTextField input = new JTextField();
+	static public JLabel message = new JLabel();
+	ChatServer server;
+	ChatClient client;
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	public ChatApp(){
+ int hosting = JOptionPane.showConfirmDialog(null, "Would You Like To Host A Chat Application Server?", "Server Activator", JOptionPane.YES_NO_OPTION);
+if(hosting == JOptionPane.YES_OPTION) {
+	server = new ChatServer(3148);
+	setTitle("SERVER");
+	JOptionPane.showMessageDialog(null, "Server started at: "+server.getIpAdress()+"\nPort: "+ server.getPort());
+    input.setEditable(true);
+    message.setBounds(0,0,775,200);
+    input.setBounds(0,250,775,200);
+    input.addActionListener((e)->{
+    	server.sendText(input.getText());
+    });
+    add(message);
+    add(input);
+    setLayout(null);
+	setVisible(true);
+    setSize(800,500);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	server.start();
+}else {
+	setTitle("CLIENT");
+	String ipAdd = JOptionPane.showInputDialog("Enter The IP Adress of Your Desired Server");
+	String servPrt = JOptionPane.showInputDialog("Enter The Port Number of Your Desired Server");
+	int port = Integer.parseInt(servPrt);
+	client = new ChatClient(ipAdd,port);
+	 input.setEditable(true);
+	    message.setBounds(0,0,775,200);
+	    input.setBounds(0,250,775,200);
+	    input.addActionListener((e)->{
+	    	client.sendText(input.getText());
+	    });
+	    add(message);
+	    add(input);
+	    setLayout(null);
+	setVisible(true);
+	setSize(800,500);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	client.start();
+}
+	}
 }
---
+++ src/_02_Chat_Application/ChatServer.java
@@ -0,0 +1,65 @@
+package _02_Chat_Application;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
+public class ChatServer {
+private int port;
+
+private ServerSocket server;
+private Socket connection;
+
+ObjectOutputStream os;
+ObjectInputStream is;
+public ChatServer(int port) {
+	this.port = port;
+}
+public void start() {
+	try {
+		server = new ServerSocket(port, 100);
+		connection = server.accept();
+		os = new ObjectOutputStream(connection.getOutputStream());
+		is = new ObjectInputStream(connection.getInputStream());
+		os.flush();
+		while(connection.isConnected()) {
+			try {
+					ChatApp.message.setText(is.readObject()+"");
+				System.out.println(is.readObject());
+			}catch(EOFException e) {
+				JOptionPane.showMessageDialog(null, "Connection To Server Lost");
+				System.exit(0);
+			}
+		}
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+public String getIpAdress() {
+	try {
+		return InetAddress.getLocalHost().getHostAddress();
+	} catch (UnknownHostException e) {
+		return "ERROR!";
+	}
+}
+public int getPort() {
+	return port;
+}
+public void sendText(String send) {
+	try {
+		if(os != null) {
+			os.writeObject(send);
+			os.flush();
+		}
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+}
+}
